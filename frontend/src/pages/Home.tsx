d20a3642ddada18574b949e8c203cded41a49bf9
Quang Truong <nqtruong.forwork@gmail.com>
2025-04-11 16:29:28 +0700

    feat(frontend): Add pages

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
+            <p className="mt-4 text-lg">
+                Welcome to the Bookworm application. This is the home page.
+            </p>
+        </div>
+    );
+}
