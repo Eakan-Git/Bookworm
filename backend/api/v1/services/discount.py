0064605e9126efe4f7d0f07edd26d63d54993fe1
Quang Truong <nqtruong.forwork@gmail.com>
2025-04-17 15:02:35 +0700

    feat (services): Add Discount

---
+++ backend/api/v1/services/discount.py
@@ -0,0 +1,11 @@
+from sqlalchemy.orm import Session
+from typing import Optional
+from models.discount import Discount
+from api.v1.schemas.discount import DiscountRead
+from datetime import date
+
+def get_current_discount_for_book(book_id: int, db: Session) -> Optional[DiscountRead]:
+    discount = db.query(Discount).filter(Discount.book_id == book_id, Discount.discount_end_date > date.today()).first()
+    if not discount:
+        return None
+    return DiscountRead.model_validate(discount)
