58868cfebc0b58b8f164a83b9fba00b5fd78d11e
Quang Truong <nqtruong.forwork@gmail.com>
2025-04-17 15:04:16 +0700

    feat (API): Add API's Paginate wrapper

---
+++ backend/api/v1/schemas/common.py
@@ -0,0 +1,14 @@
+from typing import Generic, TypeVar, List, Optional
+from pydantic import BaseModel
+from pydantic.generics import GenericModel
+
+T = TypeVar("T")
+
+class PaginationMeta(BaseModel):
+    total: int
+    page: int
+    size: int
+
+class PaginatedResponse(GenericModel, Generic[T]):
+    data: List[T]
+    meta: PaginationMeta
