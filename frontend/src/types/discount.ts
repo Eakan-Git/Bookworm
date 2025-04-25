fc1333edb00971bb28bb2be263f94c1cc610b114
Quang Truong <nqtruong.forwork@gmail.com>
2025-04-25 12:45:39 +0700

    refactor (type): discount_end_date now can be optional

--- frontend/src/types/discount.ts
+++ frontend/src/types/discount.ts
@@ -1,7 +1,7 @@
 export interface Discount {
     id: number;
     discount_start_date: Date;
-    discount_end_date: Date;
+    discount_end_date?: Date;
     discount_price: number;
     book_id: number;
 }
