20943bee77555dbc5d30eb3b90de82c660e2492d
Quang Truong <nqtruong.forwork@gmail.com>
2025-04-25 12:41:25 +0700

    refactor (DB): Review date to datetime

--- backend/models/review.py
+++ backend/models/review.py
@@ -1,9 +1,10 @@
 from sqlmodel import SQLModel, Field, Relationship
 from typing import Optional, TYPE_CHECKING
-from datetime import datetime
+from datetime import datetime, timezone
 
 if TYPE_CHECKING:
     from .book import Book
+
 
 class Review(SQLModel, table=True):
     __tablename__ = "review"
@@ -12,7 +13,9 @@ class Review(SQLModel, table=True):
     book_id: Optional[int] = Field(default=None, foreign_key="book.id")
     review_title: str = Field(max_length=120)
     review_details: Optional[str] = Field(default=None)
-    review_date: datetime = Field(default_factory=datetime.utcnow)
+    review_date: datetime = Field(
+        default_factory=lambda: datetime.now(timezone.utc)
+    )
     rating_star: Optional[int] = Field(default=None, ge=1, le=5)
 
     # Relationships
