fbdcd1e2cd3f873e7ad9be8b7c547e033d11b462
Quang Truong <nqtruong.forwork@gmail.com>
2025-04-28 11:23:25 +0700

    refactor (be/service): Return discount_end_date null discount

--- backend/api/v1/services/discount.py
+++ backend/api/v1/services/discount.py
@@ -3,11 +3,23 @@ from typing import Optional
 from models.discount import Discount
 from api.v1.schemas.discount import DiscountRead
 from datetime import date
+from sqlalchemy import or_
+
 
 class DiscountService:
     @staticmethod
     def get_current_discount_for_book(book_id: int, db: Session) -> Optional[DiscountRead]:
-        discount = db.query(Discount).filter(Discount.book_id == book_id, Discount.discount_end_date > date.today()).first()
+        discount = (
+            db.query(Discount)
+            .filter(
+                Discount.book_id == book_id,
+                or_(
+                    Discount.discount_end_date == None,
+                    Discount.discount_end_date > date.today()
+                )
+            )
+            .first()
+        )
         if not discount:
             return None
         return DiscountRead.model_validate(discount)
