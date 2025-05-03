3e88769b0d415138d3acc91503d43151fe37e66b
Quang Truong <nqtruong.forwork@gmail.com>
2025-05-04 02:48:58 +0700

    feat (UI): Retrieve reviews of a book and post review

--- frontend/src/types/book.ts
+++ frontend/src/types/book.ts
@@ -1,9 +1,11 @@
 import { Category } from "@/types/category";
 import { Author } from "@/types/author";
 import { Discount } from "@/types/discount";
+import { Paginate } from "@/types/paginate";
 
 interface Rating {
     average_rating: number;
+    review_count: number;
 }
 
 export interface Book {
@@ -16,4 +18,11 @@ export interface Book {
     discount: Discount;
     book_cover_photo: string;
     rating?: Rating;
+}
+
+export interface BookFilterParams extends Paginate {
+    category_id?: number;
+    author_id?: number;
+    rating_star?: number;
+    sort_by?: "on_sale" | "popularity" | "price_asc" | "price_desc";
 }
--- frontend/src/types/review.ts
+++ frontend/src/types/review.ts
@@ -1,11 +1,24 @@
-import {PaginatedResponse} from "@/types/paginatedResponse";
+import { PaginatedResponse } from "@/types/paginatedResponse";
+import { Paginate } from "@/types/paginate";
 
 export interface Review {
     id: number;
     book_id: number;
     review_title: string;
-    review_details: string;
+    review_details?: string;
     review_date: string;
+    rating_star: number;
+}
+
+export interface ReviewFilterParams extends Paginate {
+    rating_star?: number;
+    sort_by?: "date";
+    sort_direction?: "asc" | "desc";
+}
+
+export interface ReviewFormValues {
+    review_title: string;
+    review_details?: string;
     rating_star: number;
 }
 
